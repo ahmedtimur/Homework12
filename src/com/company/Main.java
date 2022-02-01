6d5e6e71e8023e9eea0c09cf962f90fd50a537e5
AhmedTimur <mathfun65@gmail.com>
2022-02-01 21:38:10 +0600

    Hw12

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Triangle areaOfRectangle = new Triangle();
-        areaOfRectangle.a = 11;
-        areaOfRectangle.b = 11;
-        areaOfRectangle.c = 11;
+        Triangle areaOfTriangle = new Triangle();
+        areaOfTriangle.a = 11;
+        areaOfTriangle.b = 11;
+        areaOfTriangle.c = 11;
 
-        System.out.println(areaOfRectangle.area());
+        System.out.println(areaOfTriangle.area());
 
     }
 }
