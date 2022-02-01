9b6b0b65848d4cf6d5c5c77fe7dd6964d5f502d3
AhmedTimur <mathfun65@gmail.com>
2022-02-01 21:35:27 +0600

    Hw12

--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -1,9 +1,10 @@
 package com.company;
 
 public class Triangle {
-    double a, b, c, s, info;
+    double a, b, c;
 
     double area() {
+        double s, info;
         s = (a+b+c)/2;
         info = Math.sqrt(s*(s-a)*(s-b)*(s-c));
         return info;
